b8019105c556f48103656c1481144ca028d91d53
s4iOFFL <saisabarish.s.2023.cse@ritchennai.edu.in>
2026-01-06 15:31:10 +0530

    haha

--- FileHandling.js
+++ FileHandling.js
@@ -1,9 +1,21 @@
+// fs.writeFile(path, data, callback)
+
 const fs = require('fs');
 fs.mkdir('./File', (err) => {
     if (err)
         console.log(err.message);
-    else
+    else {
         console.log('File Created');
+        fs.writeFile('./file/fs.txt'), 'Hoiiiii', (err) => {
+            if (err)
+                console.log(err.message)
+            else
+                console.log('File Written Successfully');
+        }
+
+    }
+
+
 })
 
 
