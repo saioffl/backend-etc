0be599dab5ccb69f5b3e3835b08525d83f78c706
s4iOFFL <saisabarish.s.2023.cse@ritchennai.edu.in>
2026-01-07 22:07:17 +0530

    huuh

---
+++ Buffers.js
@@ -0,0 +1,8 @@
+// Buffer.alloc(10);       // allocate memory
+// Buffer.from("data");   // from string
+// buffer.length;         // size in bytes1
+
+
+
+
+
--- FileHandling.js
+++ FileHandling.js
@@ -1,24 +1,29 @@
 // fs.writeFile(path, data, callback)
 
 const fs = require('fs');
-fs.mkdir('./File', (err) => {
-    if (err)
-        console.log(err.message);
-    else {
-        console.log('File Created');
-        fs.writeFile('./file/fs.txt', 'Hoiiiii', (err) => {
-            if (err){
-                console.log(err.message)
-            }
-                
-            else{
-                console.log('File Written Successfully');
-            }
-                
-        })
+if (!fs.existsSync('./File')) {
+    fs.mkdir('./File', (err) => {
+        if (err)
+            console.log(err.message);
+        else {
+            console.log('File Created');
+            fs.writeFile('./file/fs.txt', 'Hoiiiii', (err) => {
+                if (err) {
+                    console.log(err.message)
+                }
 
-    }
-})
+                else {
+                    console.log('File Written Successfully');
+                }
+
+            })
+
+        }
+    })
+}
+else {
+    console.log("Folder Exists")
+}
 
 
 
