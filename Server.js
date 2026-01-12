330bf5db1314dbac54d001c90a7a01f0eac055a1
s4iOFFL <saisabarish.s.2023.cse@ritchennai.edu.in>
2026-01-12 13:19:09 +0530

    server !!

--- Server.js
+++ Server.js
@@ -1,7 +1,13 @@
-const http = require('http') ;
+const http = require('http');
 
-const server = http.createServer((req,res) =>{
+const server = http.createServer((req, res) => {
     console.log('Server is Running !!')
+    console.log(req.url)
+    console.log(req.method)
+
+
 })
 
-server.listen()
+server.listen(3000, 'localhost', () => {
+    console.log('Server is Listening !!')
+})
