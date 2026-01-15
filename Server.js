44737b1a7578a181be794fa86874f9476f7a248f
s4iOFFL <saisabarish.s.2023.cse@ritchennai.edu.in>
2026-01-15 17:58:21 +0530

    Bad Practice !!!

--- Server.js
+++ Server.js
@@ -1,20 +1,17 @@
 const http = require('http')
 const fs = require('fs')
-let data = 'Hey Kunwe !!!'
 const server = http.createServer((request, response) => {
     console.log(request.url);
     if (!fs.existsSync('Https')) {
         fs.mkdir('Https', () => {
             console.log('File created !!')
-            fs.writeFile('Universal.txt' , data ,(err)=>{
-                if(err) {
+            fs.writeFile('./Https/Universal.txt', 'Heyy Kunweee !!!', (err) => {
+                if (err) {
                     console.log(err.message)
                 }
                 console.log('OverRided Message !')
-
             })
         })
-
     }
     else {
         console.log('Already Exists !')
