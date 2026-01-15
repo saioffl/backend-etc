8bcb80c4fb5fbf79b29abb38b3c8299f1181899f
s4iOFFL <saisabarish.s.2023.cse@ritchennai.edu.in>
2026-01-15 17:30:37 +0530

    doubt shi

--- Server.js
+++ Server.js
@@ -1,5 +1,27 @@
 const http = require('http')
+const fs = require('fs')
+let data = 'Hey Kunwe !!!'
+const server = http.createServer((request, response) => {
+    console.log(request.url);
+    if (!fs.existsSync('Https')) {
+        fs.mkdir('Https', () => {
+            console.log('File created !!')
+            fs.writeFile('Universal.txt' , data ,(err)=>{
+                if(err) {
+                    console.log(err.message)
+                }
+                console.log('OverRided Message !')
 
-http.createServer((request,response) =>{
-    console.log(response);
+            })
+        })
+
+    }
+    else {
+        console.log('Already Exists !')
+    }
+
+
+
 })
+
+server.listen('3000', 'localhost')
