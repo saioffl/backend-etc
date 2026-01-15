a01e682e886c2dfade5b34f1dd7838a0ea8e617d
s4iOFFL <saisabarish.s.2023.cse@ritchennai.edu.in>
2026-01-15 17:14:57 +0530

    hahha

--- Server.js
+++ Server.js
@@ -1,15 +1,5 @@
-const http = require('http');
+const http = require('http')
 
-const server = http.createServer((req, res) => {
-    console.log('Server is Running !!')
-    console.log(req.url)
-    console.log(req.method)
-
-    req.headers('content-type' , 'plain/text');
-
-
-})
-
-server.listen(3000, 'localhost', () => {
-    console.log('Server is Listening !!')
+http.createServer((request,response) =>{
+    console.log(response);
 })
