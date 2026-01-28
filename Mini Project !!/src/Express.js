f617021264565c7ed2fbd40ead0e1dbc5d9400e0
s4iOFFL <saisabarish.s.2023.cse@ritchennai.edu.in>
2026-01-28 11:14:32 +0530

    nodemon CMDS

---
+++ Mini Project !!/src/Express.js
@@ -0,0 +1,17 @@
+const exp = require('express');
+const app = exp();
+
+
+app.get('/' , (req,res) =>{
+    console.log('Entered World !')
+    res.sendFile('/index.html' , {root:__dirname}) ;
+})
+
+app.get('/home', (req, res) => {
+    console.log('Home Page !!')
+    res.sendFile('/home.html', { root: __dirname })
+})
+
+app.listen(3000, () => {
+    console.log('Server Running @ https://localhost:3000');
+})
