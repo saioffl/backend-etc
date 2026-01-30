b21a85a5ac91d3db2d9723dbca8ec6a9e9793205
s4iOFFL <saisabarish.s.2023.cse@ritchennai.edu.in>
2026-01-30 20:23:31 +0530

    huh

--- Mini Project !!/src/Express.js
+++ Mini Project !!/src/Express.js
@@ -1,6 +1,12 @@
-const exp = require('express');
+const exp = require('express')
 const app = exp();
 
+
+const userNames = [
+    {id:1 , stdName:'Sai'},
+    {id:2 , stdName:'Naga'},
+    {id:3 , stdName:'Bubu'}
+] 
 
 app.get('/' , (req,res) =>{
     console.log('Entered World !')
@@ -12,6 +18,10 @@ app.get('/home', (req, res) => {
     res.sendFile('/home.html', { root: __dirname })
 })
 
+
+
+app.use(exp.json())
+
 app.listen(3000, () => {
     console.log('Server Running @ https://localhost:3000');
 })
@@ -19,3 +29,4 @@ app.listen(3000, () => {
 app.use('/homeUS', (res,req) =>{
     console.log('Not Found !!')
 })
+
