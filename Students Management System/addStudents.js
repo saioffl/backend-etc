5e6889b0a766e4599d42e18c8c8b68caf4e0e279
s4iOFFL <saisabarish.s.2023.cse@ritchennai.edu.in>
2026-02-01 15:35:59 +0530

    huh

--- Students Management System/addStudents.js
+++ Students Management System/addStudents.js
@@ -6,4 +6,9 @@ btn.addEventListener('click', (e) => {
     const li = document.createElement('li')
     li.textContent = ip.value;
     list.appendChild(li)
+    li.style.padding = '1rem' ;
+    li.style.color = 'yellow' ;
+    li.style.fontFamily = 'Mono';
+    li.style.fontSize = '20px';
+
 })
