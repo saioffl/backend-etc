3a31b3bbed1b4627f421d2b252f9fcc204801343
s4iOFFL <saisabarish.s.2023.cse@ritchennai.edu.in>
2026-02-04 21:12:31 +0530

    Holy !!

--- Javascript Intermediate/map.js
+++ Javascript Intermediate/map.js
@@ -1,3 +1,28 @@
-const userArray = [1,2,3,4]
-userArray.map()
+const express = require('express');
+const app = express(); // Express Init ! 
+const json = express.json
+app.get('/students/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const student = stdArray.find((std) => std.id === id);
+    console.log(student)
 
+    if(!student){
+        res.status(404).send('Student Not Found')
+    }
+    res.json(student)
+})
+
+const stdArray = [
+    {
+        id: 1, Name: 'Sai'
+    },
+    {
+        id: 2, Name: 'Naga'
+    }
+]
+
+
+
+app.listen(3000, () => {
+    console.log('Server Running on Port 3000 : localHost');
+})
